2653b33d97cde62ac5f6c4ffef7f0432c593df65
draudeo <98958577+draudeo@users.noreply.github.com>
2022-11-22 21:48:03 +0800

    quiz title back btn

--- app/src/main/java/com/example/dentalqmgmtsys/Fragments/QuizTitleFragment.java
+++ app/src/main/java/com/example/dentalqmgmtsys/Fragments/QuizTitleFragment.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.dentalqmgmtsys.QuizGameActivity;
 import com.example.dentalqmgmtsys.QuizRuleActivity;
+import com.example.dentalqmgmtsys.R;
 import com.example.dentalqmgmtsys.databinding.FragmentQuizTitleBinding;
 
 
@@ -24,6 +28,18 @@ public class QuizTitleFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentQuizTitleBinding.inflate(inflater, container, false);
+
+        binding.backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment newFragment = new QueueFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_layout, newFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
         binding.playBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
