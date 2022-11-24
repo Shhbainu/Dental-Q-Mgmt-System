3ece42da4260d21ac5639b27661c91cad8739495
Joyurizzz <65898468+Joyurizzz@users.noreply.github.com>
2022-11-24 21:04:36 +0800

    updates in appointment fragments

--- app/src/main/java/com/example/dentalqmgmtsys/Adapter/MyViewPagerAdapter.java
+++ app/src/main/java/com/example/dentalqmgmtsys/Adapter/MyViewPagerAdapter.java
@@ -2,16 +2,12 @@ package com.example.dentalqmgmtsys.Adapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-import com.example.dentalqmgmtsys.AddAppointmentActivity;
 import com.example.dentalqmgmtsys.Fragments.AddAppointmentStep1Fragment;
 import com.example.dentalqmgmtsys.Fragments.AddAppointmentStep2Fragment;
 import com.example.dentalqmgmtsys.Fragments.AddAppointmentStep3Fragment;
-import com.example.dentalqmgmtsys.Fragments.AddAppointmentStep4Fragment;
 
 public class MyViewPagerAdapter extends FragmentPagerAdapter {
 
@@ -23,14 +19,12 @@ public class MyViewPagerAdapter extends FragmentPagerAdapter {
     @Override
     public Fragment getItem(int i) {
         switch (i){
-/*            case 0:
-                return AddAppointmentStep1Fragment.getInstance();*/
             case 0:
-                return AddAppointmentStep2Fragment.getInstance();
+                return AddAppointmentStep1Fragment.getInstance();
             case 1:
-                return AddAppointmentStep3Fragment.getInstance();
+                return AddAppointmentStep2Fragment.getInstance();
             case 2:
-                return AddAppointmentStep4Fragment.getInstance();
+                return AddAppointmentStep3Fragment.getInstance();
         }
         return null;
     }
--- app/src/main/java/com/example/dentalqmgmtsys/Fragments/AddAppointmentStep3Fragment.java
+++ app/src/main/java/com/example/dentalqmgmtsys/Fragments/AddAppointmentStep3Fragment.java
@@ -1,6 +1,5 @@
 package com.example.dentalqmgmtsys.Fragments;
 
-import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,157 +8,152 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.dentalqmgmtsys.Adapter.MyTimeSlotAdapter;
 import com.example.dentalqmgmtsys.Common.Common;
-import com.example.dentalqmgmtsys.Common.SpacesItemDecoration;
-import com.example.dentalqmgmtsys.Interface.ITimeSlotLoadListener;
-import com.example.dentalqmgmtsys.Models.TimeSlot;
+import com.example.dentalqmgmtsys.Models.AppointmentInformation;
 import com.example.dentalqmgmtsys.R;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.CollectionReference;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
-import devs.mulham.horizontalcalendar.HorizontalCalendar;
-import devs.mulham.horizontalcalendar.HorizontalCalendarView;
-import devs.mulham.horizontalcalendar.utils.HorizontalCalendarListener;
-import dmax.dialog.SpotsDialog;
 
-public class AddAppointmentStep3Fragment extends Fragment implements ITimeSlotLoadListener {
-
-    //Variable
-    DocumentReference slotRef;
-    ITimeSlotLoadListener iTimeSlotLoadListener;
-    AlertDialog dialog;
-
-    Unbinder unbinder;
-    LocalBroadcastManager localBroadcastManager;
-
-    @BindView(R.id.timeSlotRV)
-    RecyclerView timeSlotRV;
-    @BindView(R.id.calendarView)
-    HorizontalCalendarView calendarView;
+public class AddAppointmentStep3Fragment extends Fragment {
 
     SimpleDateFormat simpleDateFormat;
+    LocalBroadcastManager localBroadcastManager;
+    FirebaseAuth firebaseAuth;
+    DatabaseReference databaseReference;
 
-    BroadcastReceiver displayTimeSlot = new BroadcastReceiver() {
+    @BindView(R.id.confirmNameTV)
+    TextView confirmNameTV;
+    @BindView(R.id.confirmPhoneTV)
+    TextView confirmPhoneTV;
+    @BindView(R.id.confirmDoctorTV)
+    TextView confirmDoctorTV;
+    @BindView(R.id.confirmDateTimeTV)
+    TextView confirmDateTimeTV;
+    @BindView(R.id.confirmServiceTV)
+    TextView confirmServiceTV;
+    @BindView(R.id.finalConfirmButton)
+    Button finalConfirmButton;
+    @OnClick(R.id.finalConfirmButton)
+    void confirmAppointment(){
+
+        //Creating appointment information
+        AppointmentInformation appointmentInformation = new AppointmentInformation();
+
+        appointmentInformation.setDoctor(Common.currentDoctor);
+        appointmentInformation.setService(Common.currentService.getName());
+        appointmentInformation.setTime(Common.convertTimeSlotToString(Common.currentTimeSlot));
+        appointmentInformation.setDate(simpleDateFormat.format(Common.currentDate.getTime()).toString());
+        appointmentInformation.setSlot((long) Common.currentTimeSlot);
+        appointmentInformation.setPatientName(Common.currentUser);
+        appointmentInformation.setPatientPhone(Common.currentPhone);
+
+        //Submit Firestore
+        DocumentReference appointmentDate = FirebaseFirestore.getInstance()
+                .collection("AllDoctors")
+                .document(Common.currentDoctor)
+                .collection(Common.simpleFormatDate.format(Common.currentDate.getTime()))
+                .document(String.valueOf(Common.currentTimeSlot));
+
+        //Write data
+        appointmentDate.set(appointmentInformation)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        saveToRTD();
+                        resetStaticData();
+                        getActivity().finish();
+                        Toast.makeText(getContext(), "Success!", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getContext(), ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void saveToRTD() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        String uid = firebaseAuth.getUid();
+        AppointmentInformation appointmentInformation = new AppointmentInformation();
+
+        appointmentInformation.setDoctor(Common.currentDoctor);
+        appointmentInformation.setService(Common.currentService.getName());
+        appointmentInformation.setTime(Common.convertTimeSlotToString(Common.currentTimeSlot));
+        appointmentInformation.setDate(simpleDateFormat.format(Common.currentDate.getTime()).toString());
+        appointmentInformation.setSlot((long) Common.currentTimeSlot);
+        appointmentInformation.setPatientName(Common.currentUser);
+        appointmentInformation.setPatientPhone(Common.currentPhone);
+
+        databaseReference = FirebaseDatabase.getInstance("https://dental-qmgmt-system-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                .getReference("Users");
+        databaseReference.child(uid).child("appointments").child(Common.simpleFormatDate.format(Common.currentDate.getTime())).child((String.valueOf(Common.currentTimeSlot)))
+                .setValue(appointmentInformation)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getContext(), ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+
+    }
+
+
+    private void resetStaticData() {
+        Common.step = 0;
+        Common.currentTimeSlot = -1;
+        Common.currentService = null;
+        Common.currentDoctor = null;
+        Common.currentDate.add(Calendar.DATE, 0); //Current Date
+    }
+
+    Unbinder unbinder;
+
+
+    BroadcastReceiver confirmAppointingReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Calendar date = Calendar.getInstance();
-            date.add(Calendar.DATE, 0);//Add current date
-            loadAvailableTimeSlotOfDentist(Common.currentDoctor,
-                    simpleDateFormat.format(date.getTime()));
+            setData();
         }
     };
 
-    private void loadAvailableTimeSlotOfDentist(String name, final String bookDate) {
-
-        ///AllDoctors/Maria Clara/TimeSlot
-        slotRef = FirebaseFirestore.getInstance()
-                .collection("AllDoctors")
-                .document(name);
-
-        slotRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if(task.isSuccessful()){
-                    DocumentSnapshot documentSnapshot = task.getResult();
-                    if(documentSnapshot.exists()) //If dentist is available
-                    {
-                        //Get information of appointments and load in cardView
-                        //If not exist, return empty
-                        CollectionReference date = FirebaseFirestore.getInstance()
-                                .collection("AllDoctors")
-                                .document(name)
-                                .collection(bookDate);
-
-                        date.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                if(task.isSuccessful()){
-                                    QuerySnapshot querySnapshot = task.getResult();
-                                    if(querySnapshot.isEmpty())//If doesn't have any appointment
-                                        iTimeSlotLoadListener.onTimeSlotLoadEmpty();
-                                    else
-                                    {
-                                        //If appointments exists
-                                        List<TimeSlot> timeSlots= new ArrayList<>();
-                                        for(QueryDocumentSnapshot document: task.getResult())
-                                            timeSlots.add(document.toObject(TimeSlot.class));
-                                        iTimeSlotLoadListener.onTimeSlotLoadSuccess(timeSlots);
-                                    }
-                                }
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                iTimeSlotLoadListener.onTimeSlotLoadFailed(e.getMessage());
-                            }
-                        });
-                    }
-                }
-            }
-        });
-
-
-
-/*        slotRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful()){
-                    //Get information of appointment
-                    CollectionReference dateAppoint = FirebaseFirestore.getInstance()
-                            .collection("AllDoctors")
-                            .document(Common.currentDoctor)
-                            .collection(date);
-
-                    dateAppoint.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                            if(task.isSuccessful()){
-                                QuerySnapshot querySnapshot = task.getResult();
-                                if(querySnapshot.isEmpty()) // If dont have any appointment
-                                    iTimeSlotLoadListener.onTimeSlotLoadEmpty();
-                                else
-                                {
-                                    List<TimeSlot> timeSlots = new ArrayList<>();
-                                    for(QueryDocumentSnapshot documentSnapshot1 : task.getResult())
-                                        timeSlots.add(documentSnapshot1.toObject(TimeSlot.class));
-                                    iTimeSlotLoadListener.onTimeSlotLoadSuccess(timeSlots);
-                                }
-                            }
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            iTimeSlotLoadListener.onTimeSlotLoadFailed(e.getMessage());
-                        }
-                    });
-                }
-            }
-        });*/
+    private void setData() {
+        confirmNameTV.setText(Common.currentUser);
+        confirmPhoneTV.setText(Common.currentPhone);
+        confirmDoctorTV.setText(Common.currentDoctor);
+        confirmServiceTV.setText(Common.currentService.getName());
+        confirmDateTimeTV.setText(new StringBuilder(Common.convertTimeSlotToString(Common.currentTimeSlot))
+                .append(" at ")
+                .append(simpleDateFormat.format(Common.currentDate.getTime())));
 
     }
 
@@ -171,23 +165,23 @@ public class AddAppointmentStep3Fragment extends Fragment implements ITimeSlotLo
         return instance;
     }
 
+
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        iTimeSlotLoadListener = this;
+        //format of date display
+        simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy");
 
         localBroadcastManager = LocalBroadcastManager.getInstance(getContext());
-        localBroadcastManager.registerReceiver(displayTimeSlot, new IntentFilter(Common.KEY_DISPLAY_TIME_SLOT));
-
-        simpleDateFormat = new SimpleDateFormat("MM_dd_yyyy"); // 11_22_2022 (primary key)
-
-        dialog = new SpotsDialog.Builder().setContext(getContext()).setCancelable(false).build();
+        localBroadcastManager.registerReceiver(confirmAppointingReceiver, new IntentFilter(Common.KEY_CONFIRM_APPOINTMENT));
+        
     }
 
     @Override
     public void onDestroy() {
-        localBroadcastManager.unregisterReceiver(displayTimeSlot);
+        localBroadcastManager.unregisterReceiver(confirmAppointingReceiver);
         super.onDestroy();
     }
 
@@ -196,67 +190,10 @@ public class AddAppointmentStep3Fragment extends Fragment implements ITimeSlotLo
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
 
-        View itemView = inflater.inflate(R.layout.fragment_add_appointment_step3,container, false);
+        View itemView = inflater.inflate(R.layout.fragment_add_appointment_step4,container, false);
         unbinder = ButterKnife.bind(this, itemView);
 
-        init(itemView);
         return itemView;
-
-    }
-
-    private void init(View itemView) {
-        timeSlotRV.setHasFixedSize(true);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 3);
-        timeSlotRV.setLayoutManager(gridLayoutManager);
-        timeSlotRV.addItemDecoration(new SpacesItemDecoration(8));
-
-        //Calendar
-        Calendar startDate = Calendar.getInstance();
-        startDate.add(Calendar.DATE, 0);
-        Calendar endDate = Calendar.getInstance();
-        endDate.add(Calendar.DATE, 31); // 31 day left
-
-        HorizontalCalendar horizontalCalendar = new HorizontalCalendar.Builder(itemView, R.id.calendarView)
-                .range(startDate, endDate)
-                .datesNumberOnScreen(1)
-                .mode(HorizontalCalendar.Mode.DAYS)
-                .defaultSelectedDate(startDate)
-                .build();
-        horizontalCalendar.setCalendarListener(new HorizontalCalendarListener() {
-            @Override
-            public void onDateSelected(Calendar date, int position) {
-                if(Common.currentDate.getTimeInMillis() != date.getTimeInMillis())
-                {
-                    Common.currentDate = date; // This will not load again if selected
-                    loadAvailableTimeSlotOfDentist(Common.currentDoctor,
-                            simpleDateFormat.format(date.getTime()));
-                }
-            }
-        });
-
-    }
-
-    @Override
-    public void onTimeSlotLoadSuccess(List<TimeSlot> timeSlotList) {
-        MyTimeSlotAdapter adapter = new MyTimeSlotAdapter(getContext(), timeSlotList);
-        timeSlotRV.setAdapter(adapter);
-
-        dialog.dismiss();
-
-    }
-
-    @Override
-    public void onTimeSlotLoadFailed(String message) {
-        Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
-        dialog.dismiss();
-    }
-
-    @Override
-    public void onTimeSlotLoadEmpty() {
-        MyTimeSlotAdapter adapter = new MyTimeSlotAdapter(getContext());
-        timeSlotRV.setAdapter(adapter);
-
-        dialog.dismiss();
 
     }
 }
