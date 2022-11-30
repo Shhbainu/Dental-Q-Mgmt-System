d8a63a7399580fa7316df4f848f981fccc2cc4b3
Riicchie <42201290+Riicchie23@users.noreply.github.com>
2022-11-30 11:16:06 +0800

    Update QueueFragment.java

--- app/src/main/java/com/example/dentalqmgmtsys/Fragments/QueueFragment.java
+++ app/src/main/java/com/example/dentalqmgmtsys/Fragments/QueueFragment.java
@@ -37,8 +37,8 @@ public class QueueFragment extends Fragment {
     String channelID = "1234";
     String description = "Test Notification";
     //Viewbinding
-    private FragmentQueueBinding binding;               //Haven't added Notifs
-    //Variables
+    private FragmentQueueBinding binding;               //Haven't added Notifs - done
+    //Variables                                         //Next connecting database and finishing adding of time
     private Long comTime;
     private Long remainTime;                            //There's a bug when pressing recent apps, the timer bugs out
     private Long timeLeft;                              //If encountered this bug, just close the app
